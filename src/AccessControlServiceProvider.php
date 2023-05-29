a5db5442a343ce4829f1242dbb1c9a7eb0398201
Back2Lobby <tayyabisking1101@gmail.com>
2023-05-29 15:30:11 +0500

    fixed default config not loading, fixed migrations

--- src/AccessControlServiceProvider.php
+++ src/AccessControlServiceProvider.php
@@ -16,85 +16,79 @@ use Illuminate\Support\ServiceProvider;
 
 class AccessControlServiceProvider extends ServiceProvider
 {
-    public function provides(): array
-    {
-        return ['access-control'];
-    }
+	/**
+	 * Register services.
+	 */
+	public function register(): void
+	{
+		$this->mergeConfigFrom(__DIR__ . '/../config/access.php', 'access');
+	}
 
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-    }
+	public function boot(): void
+	{
 
-    public function boot(): void
-    {
+		// add migrations
+		$this->publishMigrations();
 
-        // add migrations
-        $this->publishMigrations();
+		// add configs
+		$this->publishConfigs();
 
-        // add configs
-        $this->publishConfigs();
+		// create singleton
+		$this->app->singleton('access-control', function ($app) {
+			return new AccessControlService(CacheStore::getInstance(), SessionStore::getInstance());
+		});
 
-        // if (!App::runningInConsole()) {
-        // create singleton
-        $this->app->singleton('access-control', function ($app) {
-            return new AccessControlService(CacheStore::getInstance(), SessionStore::getInstance());
-        });
+		// setup check point
+		$this->app->make(Gate::class)->before(function (Model $user, $authority, array $arguments = []) {
+			return $this->accessControlCheck($user, $authority, $arguments);
+		});
 
-        // setup check point
-        $this->app->make(Gate::class)->before(function (Model $user, $authority, array $arguments = []) {
-            return $this->accessControlCheck($user, $authority, $arguments);
-        });
+		// set currently auth user in session store
+		Event::listen(Authenticated::class, function (Authenticated $event) {
+			$this->app->get('access-control')?->getSessionStore()->setAuthUser($event->user);
+		});
+	}
 
-        // set currently auth user in session store
-        Event::listen(Authenticated::class, function (Authenticated $event) {
-            $this->app->get('access-control')?->getSessionStore()->setAuthUser($event->user);
-        });
-        // }
-    }
+	/**
+	 * Publish the package's migrations.
+	 */
+	protected function publishMigrations(): void
+	{
+		$timestamp = date('Y_m_d_His', time());
 
-    /**
-     * Publish the package's migrations.
-     */
-    protected function publishMigrations(): void
-    {
-        $timestamp = date('Y_m_d_His', time());
+		$stub = __DIR__ . '/../migrations/create_access_control_tables.php';
 
-        $stub = __DIR__.'/../migrations/create_access_control_tables.php';
+		$target = $this->app->databasePath() . '/migrations/' . $timestamp . '_create_access_control_tables.php';
 
-        $target = $this->app->databasePath().'/migrations/'.$timestamp.'_create_access_control_tables.php';
+		$this->publishes([$stub => $target], 'access-control.migrations');
+	}
 
-        $this->publishes([$stub => $target], 'access-control.migrations');
-    }
+	protected function publishConfigs(): void
+	{
+		$this->publishes([
+			__DIR__ . '/../config/access.php' => $this->app->configPath() . '/access.php',
+		], 'access-control.config');
+	}
 
-    protected function publishConfigs(): void
-    {
-        $this->publishes([
-            __DIR__.'/../config/access.php' => $this->app->configPath().'/access.php',
-        ], 'access-control.config');
-    }
+	/**
+	 * Check whether the user can do this or not
+	 *
+	 * @throws InvalidRoleableException
+	 */
+	protected function accessControlCheck($user, $authority, array $arguments = []): bool
+	{
+		$roleable = isset($arguments[0]) > 0 ? $arguments[0] : null;
 
-    /**
-     * Check whether the user can do this or not
-     *
-     * @throws InvalidRoleableException
-     */
-    protected function accessControlCheck($user, $authority, array $arguments = []): bool
-    {
-        $roleable = isset($arguments[0]) > 0 ? $arguments[0] : null;
+		// if roleable is string then assuming it's currently authorizing for a route
+		if (is_string($roleable) && class_exists($roleable) && Route::current()) {
+			$roleableId = Route::current()->parameter(strtolower(class_basename($roleable)));
+			if ($roleableId) {
+				$roleable = $roleable::find($roleableId);
+			} else {
+				throw new InvalidRoleableException("The `id` of the roleable `$roleable` was not found in the route URL. Please make sure to pass the `id` of the `$roleable` instance through the URL parameter.");
+			}
+		}
 
-        // if roleable is string then assuming it's currently authorizing for a route
-        if (is_string($roleable) && class_exists($roleable) && Route::current()) {
-            $roleableId = Route::current()->parameter(strtolower(class_basename($roleable)));
-            if ($roleableId) {
-                $roleable = $roleable::find($roleableId);
-            } else {
-                throw new InvalidRoleableException("The `id` of the roleable `$roleable` was not found in the route URL. Please make sure to pass the `id` of the `$roleable` instance through the URL parameter.");
-            }
-        }
-
-        return AccessControlFacade::canUser($user, true)->do($authority, $roleable) ?? false;
-    }
+		return AccessControlFacade::canUser($user, true)->do($authority, $roleable) ?? false;
+	}
 }
